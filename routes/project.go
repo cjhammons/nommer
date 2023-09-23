12cdd505f2f94c961444bd37a3024a711c1b10e1
Curtis Hammons <chammons@boingo.com>
2023-09-23 10:16:30 -0700

    implemented routes for project and event

--- routes/project.go
+++ routes/project.go
@@ -5,6 +5,13 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"math/big"
+	"time"
+
 	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -13,48 +20,114 @@ import (
 type Project struct {
 	ID     string  `json:"id" bson:"_id,omitempty"`
 	Name   string  `json:"name" bson:"name"`
-	APIKEY string  `json:"apikey" bson:"apikey"`
+	APIKey string  `json:"apikey" bson:"apikey"`
 	Events []Event `json:"events" bson:"events"`
 }
 
 type Event struct {
-	Type string                 `json:"type" bson:"type"`
-	Data map[string]interface{} `json:"data" bson:"data"`
+	TimeStamp time.Time              `json:"type" bson:"type"`
+	Data      map[string]interface{} `json:"data" bson:"data"`
 }
 
+// CreateProjectHandler creates a new project and assigns an API key
 func CreateProjectHandler(collection *mongo.Collection) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var project Project
-		err := json.NewDecoder(r.Body).Decode(&project)
-		if err != nil {
+		var payload map[string]string
+		if err := json.NewDecoder(r.Body).Decode(&payload); err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
-		insertResult, err := collection.InsertOne(context.Background(), project)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
+
+		// Extract project name from payload
+		projectName, exists := payload["name"]
+		if !exists {
+			http.Error(w, "Project name is required", http.StatusBadRequest)
 			return
 		}
-		json.NewEncoder(w).Encode(insertResult)
+
+		// Check if project name already exists
+		var existingProject Project
+		err := collection.FindOne(context.TODO(), bson.M{"name": projectName}).Decode(&existingProject)
+		if err == nil {
+			http.Error(w, "Project name already exists", http.StatusConflict)
+			return
+		}
+
+		// Create a Project object
+		var p Project
+		p.Name = projectName
+
+		// Generate an API key for the project
+		p.APIKey = GenerateAPIKey(projectName)
+
+		// Insert the project into the database
+		_, err = collection.InsertOne(context.TODO(), p)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		// Return the created project, including the API key
+		json.NewEncoder(w).Encode(p)
 	}
 }
 
 func SendProjectEventHandler(collection *mongo.Collection) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
-		var event Event
-		err := json.NewDecoder(r.Body).Decode(&event)
+		projectName := vars["projectName"]
+
+		// Retrieve API key from request header
+		apiKey := r.Header.Get("X-API-Key")
+
+		// Find project by name and validate API key
+		var foundProject Project
+		err := collection.FindOne(context.TODO(), bson.M{"name": projectName}).Decode(&foundProject)
 		if err != nil {
+			http.Error(w, "Project not found", http.StatusNotFound)
+			return
+		}
+		if foundProject.APIKey != apiKey {
+			http.Error(w, "Invalid API key", http.StatusUnauthorized)
+			return
+		}
+
+		// Decode the incoming JSON into a map
+		var payload map[string]interface{}
+		if err := json.NewDecoder(r.Body).Decode(&payload); err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
-		filter := bson.D{{"_id", vars["id"]}}
-		update := bson.D{{"$push", bson.D{{"events", event}}}}
-		updateResult, err := collection.UpdateOne(context.Background(), filter, update)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
+
+		// Check if 'event' key exists
+		eventData, exists := payload["event"]
+		if !exists {
+			http.Error(w, "Event data is required", http.StatusBadRequest)
 			return
 		}
-		json.NewEncoder(w).Encode(updateResult)
+
+		// Create the Event object
+		e := Event{
+			Data:      eventData.(map[string]interface{}),
+			TimeStamp: time.Now(),
+		}
+
+		// Push the event into MongoDB
+		update := bson.D{{"$push", bson.D{{"events", e}}}}
+		_, err = collection.UpdateOne(context.TODO(), bson.M{"name": projectName}, update)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		// Return the created event
+		json.NewEncoder(w).Encode(e)
 	}
+}
+
+func GenerateAPIKey(projectName string) string {
+	max := big.NewInt(1<<31 - 1)
+	data := fmt.Sprintf("%s-%d-%d", projectName, time.Now().UnixNano(), rand.Int(rand.Reader, max))
+	hash := sha256.Sum256([]byte(data))
+	return hex.EncodeToString(hash[:])
 }
