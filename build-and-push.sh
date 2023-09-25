64e76ee5cf7b275806d4e181100766b10b97713f
Curtis Hammons <chammons@boingo.com>
2023-09-24 21:26:34 -0700

    fixed build script typos

--- build-and-push.sh
+++ build-and-push.sh
@@ -1,2 +1,9 @@
-docker build ---platform linux/amd64 t cjhammons/nommer:latest .
-docker push cjhammons/nommer:latest #requires auth
+#!/bin/bash
+REV="latest"
+if [ "$1" != "" ]; then
+  REV=$1
+fi
+echo "Using tag $REV"
+
+docker build --platform linux/amd64 -t cjhammons/nommer:$REV .
+docker push cjhammons/nommer:$REV #requires auth
